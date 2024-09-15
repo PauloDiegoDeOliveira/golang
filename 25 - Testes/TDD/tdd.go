fa1e4deabfcc78b20690c9e00f985c12d6d19ece
Paulo Diego <pdiegodo@gmail.com>
2024-09-15 15:42:11 -0300

    TDD passo 2 Codigo Minimo para Passar no Teste

--- 25 - Testes/TDD/tdd.go
+++ 25 - Testes/TDD/tdd.go
@@ -1,22 +1,10 @@
 package tdd
 
-import "fmt"
-
 // Função Soma que recebe um slice de números e retorna a soma deles
 func Soma(numeros []int) int {
-	// Log para mostrar os números que estamos somando
-	fmt.Printf("Somando os números: %v\n", numeros)
-
-	// Variável para armazenar a soma
 	total := 0
-
-	// Itera sobre o slice de números e calcula a soma
 	for _, numero := range numeros {
 		total += numero
-		// Log para mostrar a soma parcial
-		fmt.Printf("Somando %d, total até agora: %d\n", numero, total)
 	}
-
-	// Retorna a soma total
 	return total
 }
