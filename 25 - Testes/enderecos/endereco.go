a9a22464c0cfa1720dbbed7a33f29e469c382902
Paulo Diego <pdiegodo@gmail.com>
2024-09-15 12:50:44 -0300

    Criando Função para Teste

--- 25 - Testes/enderecos/endereco.go
+++ 25 - Testes/enderecos/endereco.go
@@ -1,29 +1,23 @@
 package enderecos
 
 import (
+	"fmt"
 	"strings"
 )
 
-// Função que retorna o tipo de endereço com base nas palavras
+// TipoEndereco identifica o tipo de um endereço (ex: Rua, Avenida, Alameda, etc.)
 func TipoEndereco(endereco string) string {
-	tiposValidos := map[string]string{
-		"Rua":      "Residencial",
-		"Avenida":  "Comercial",
-		"Rodovia":  "Rodovia",
-		"Via":      "Via",
-		"Alameda":  "Residencial",
-		"Estrada":  "Estrada",
-		"Travessa": "Travessa",
-		"Quadra":   "Residencial",
+	// Pega a primeira palavra do endereço
+	primeiraPalavra := strings.Split(endereco, " ")[0]
+
+	// Log para indicar qual foi a primeira palavra do endereço
+	fmt.Println("A primeira palavra do endereço é:", primeiraPalavra)
+
+	// Verifica o tipo de endereço
+	switch primeiraPalavra {
+	case "Rua", "Avenida", "Alameda":
+		return primeiraPalavra
+	default:
+		return "Tipo Inválido"
 	}
-
-	palavras := strings.Split(endereco, " ")
-
-	for _, palavra := range palavras {
-		if tipo, existe := tiposValidos[palavra]; existe {
-			return tipo
-		}
-	}
-
-	return "Tipo desconhecido"
 }
